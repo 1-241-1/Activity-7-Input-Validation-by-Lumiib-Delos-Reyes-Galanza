5df549911ae572385d78d49645692fb75106ee3c
Dan Rey Lumiib <dglumiib@carc.pshs.edu.ph>
2026-09-22 19:45:55 +0800

    Update Actvity-7_Lumiib,DelosReyes,Galanza.pyi

--- Actvity-7_Lumiib,DelosReyes,Galanza.pyi
+++ Actvity-7_Lumiib,DelosReyes,Galanza.pyi
@@ -28,7 +28,7 @@ print("School Grade Level Validator")
 print("<=======================>")
 
 valid_grade_level = [7, 8, 9, 10, 11, 12]
-grade_level = int(input("Enter grade level"))
+grade_level = int(input("Enter grade level: "))
 
 if grade_level in valid_grade_level:
     print("Valid grade level")
