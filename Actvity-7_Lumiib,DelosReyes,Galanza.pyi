34268e113b2f81af15d97bdd4e268c43dcaab809
Dan Rey Lumiib <dglumiib@carc.pshs.edu.ph>
2026-09-22 19:44:55 +0800

    Refactor and correct input validation scripts

--- Actvity-7_Lumiib,DelosReyes,Galanza.pyi
+++ Actvity-7_Lumiib,DelosReyes,Galanza.pyi
@@ -27,7 +27,7 @@ print("<=======================>")
 print("School Grade Level Validator")
 print("<=======================>")
 
-valid_grade_level = ["7, 8 , 9, 10, 11, 12"]
+valid_grade_level = [7, 8, 9, 10, 11, 12]
 grade_level = int(input("Enter grade level"))
 
 if grade_level in valid_grade_level:
